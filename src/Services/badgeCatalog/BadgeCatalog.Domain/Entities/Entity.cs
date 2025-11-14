4a3b599250cc75a5a41ffde873672feb565eb796
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-11-14 17:41:59 -0300

    finish class base

--- src/Services/badgeCatalog/BadgeCatalog.Domain/Entities/Entity.cs
+++ src/Services/badgeCatalog/BadgeCatalog.Domain/Entities/Entity.cs
@@ -1,11 +1,31 @@
 namespace BadgeCatalog.Domain.Entities;
 
-public abstract class Entity
+public abstract class Entity : IEquatable<Entity>
 {
-    public Guid Id { get; protected set; }
-
-    protected Entity()
+    #region Properties
+    public Guid Id { get; private set; }
+    #endregion
+    #region Constructors
+    public Entity()
     {
         Id = Guid.NewGuid();
     }
+    #endregion
+    #region Methods
+    public bool Equals(Entity? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Id.Equals(other.Id);
+    }
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as Entity);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+    #endregion
 }
